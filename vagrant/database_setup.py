84e168abe14ffd0c8d17b672761445b42d4facba
Johannes_1 <Johannes.Birk11@web.de>
2020-06-10 11:35:39 +0200

    formatting code

--- vagrant/database_setup.py
+++ vagrant/database_setup.py
@@ -31,11 +31,11 @@ class MenuItem(Base):
     @property
     def serialize(self):
         return {
-           'name': self.name,
-           'description': self.description,
-           'id': self.id,
-           'price': self.price,
-           'course': self.course,
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
         }
 
 
